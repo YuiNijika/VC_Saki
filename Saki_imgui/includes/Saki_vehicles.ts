d4a10fea52446fa0a8f3748c84dbc8b55ec1ed06
ShuShuicu <Tomori@miomoe.cn>
2025-06-26 19:18:44 +0800

    Update Saki_vehicles.ts

--- Saki_imgui/includes/Saki_vehicles.ts
+++ Saki_imgui/includes/Saki_vehicles.ts
@@ -1,12 +1,14 @@
-const ImgPath = "CLEO/Saki_imgui/assets/vehicles/";
+const ImagePath = "CLEO/Saki_imgui/assets/vehicles";
 
 export const VEHICLES = {
     Infernus: { 
-        modelId: 141, 
-        img: ImgPath + "infernus.jpg"
+        modelId: 141,
+        image: ImagePath + "/infernus.jpg"
     },
     Rhino: { 
-        modelId: 162, 
-        img: ImgPath + "rhino.jpg"
+        modelId: 162,
+        image: ImagePath + "/rhino.jpg"
     }
 } as const;
+
+export type VehicleType = keyof typeof VEHICLES;
